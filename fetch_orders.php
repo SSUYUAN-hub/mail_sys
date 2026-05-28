41c3126b936863b8ca6d821fc6e163bf4643f90b
SSU-YUAN <biosvlsi0103@gmail.com>
2026-05-28 10:27:11 +0800

    修復歸檔按鈕錯誤問題-fetch_orders

--- fetch_orders.php
+++ fetch_orders.php
@@ -27,17 +27,7 @@ if (!$forceRefresh
 require_once __DIR__ . '/mail_fetcher.php';
 require_once __DIR__ . '/order_parser.php';
 
-// header('Content-Type: text/html; charset=utf-8');
-// $fetcher = new MailFetcher();
-// $unreadMails = $fetcher->fetchUnreadMails();
 
-// foreach ($unreadMails as $mail) {
-//     echo '<hr><b>ID: ' . $mail['id'] . ' | 主旨: ' . htmlspecialchars($mail['subject']) . '</b>';
-//     echo '<pre style="font-size:11px; background:#f0f0f0; padding:10px; overflow:auto;">';
-//     echo htmlspecialchars($mail['html_body']);
-//     echo '</pre>';
-// }
-// die();
 
 function scoreOrder($order) {
     $score = 0;
@@ -53,9 +43,6 @@ function scoreOrder($order) {
 try {
     $fetcher     = new MailFetcher();
     $unreadMails = $fetcher->fetchUnreadMails();
-
- header('Content-Type: text/html; charset=utf-8');
- 
 
     $orders = [];
     foreach ($unreadMails as $mail) {
